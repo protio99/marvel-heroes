26f67255d78570ab15ecdd390debb0a1378efa56
Daniela Correa Muñoz <dacomu24@gmail.com>
2023-03-27 17:34:29 -0500

    Boton de añadir comic a favoritos y guardado a local storage

--- src/components/ComicModal.jsx
+++ src/components/ComicModal.jsx
@@ -2,18 +2,53 @@ import React, { useState, useEffect } from "react";
 import "./comicModal.css";
 
 export default function ComicModal({ data, setModalHide }) {
+  const [validationComicAdded, setValidationComicAdded] = useState(false);
   const [comicData, setComicData] = useState({
     thumbnail: { path: "" },
   });
-  // const [modalHideComic, setModalHideComic] = useState(modalHide);
 
   useEffect(() => {
+    const storageFavourites = JSON.parse(localStorage.getItem("favourites"));
     if (!data) {
       return;
     }
-
     setComicData(data);
+
+    if (!storageFavourites) {
+      localStorage.setItem("favourites", JSON.stringify([]));
+    } else {
+      const validationAdded = storageFavourites.filter(
+        (comic) => comic.id === comicData.id
+      );
+      console.log("------------", validationAdded);
+
+      if (validationAdded.length !== 0) {
+        setValidationComicAdded(true);
+      }
+    }
   }, [data]);
+  useEffect(() => {
+    const storageFavourites = JSON.parse(localStorage.getItem("favourites"));
+    if (!storageFavourites) {
+      localStorage.setItem("favourites", JSON.stringify([]));
+    } else {
+      const validationAdded = storageFavourites.filter(
+        (comic) => comic.id === comicData.id
+      );
+      console.log("------------", validationAdded);
+
+      if (validationAdded.length !== 0) {
+        setValidationComicAdded(true);
+      }
+    }
+  }, [comicData]);
+
+  const addToFavourites = () => {
+    setValidationComicAdded(true);
+    const storageFavourites = JSON.parse(localStorage.getItem("favourites"));
+    storageFavourites.push(comicData);
+    localStorage.setItem("favourites", JSON.stringify(storageFavourites));
+  };
 
   return (
     <div className="modal-bg">
@@ -36,14 +71,28 @@ export default function ComicModal({ data, setModalHide }) {
           </div>
         </div>
         <div className="comic-modal__btns">
-          <button className="comic-modal__btns__add">
-            <img
-              src="/assets/icons/btn-favourites-default.png"
-              className="comic-modal__btns--icon"
-            ></img>
-            <p>Add to favourites</p>
-          </button>
-          <button className="comic-modal__btns__buy">
+          {!validationComicAdded ? (
+            <button
+              className="comic-modal__btns__btn comic-modal__btns__add"
+              onClick={() => addToFavourites()}
+            >
+              <img
+                src="/assets/icons/btn-favourites-default.png"
+                className="comic-modal__btns--icon"
+              ></img>
+              <p>Add to favourites</p>
+            </button>
+          ) : (
+            <button className="comic-modal__btns__btn comic-modal__btns__added">
+              <img
+                src="/assets/icons/btn-favourites-primary.png"
+                className="comic-modal__btns--icon"
+              ></img>
+              <p>Added to favourites</p>
+            </button>
+          )}
+
+          <button className="comic-modal__btns__btn comic-modal__btns__buy">
             <img
               src="/assets/icons/shopping-cart-primary.png"
               className="comic-modal__btns--icon"
